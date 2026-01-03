428223373a844a8c8490d4d3e84d0ca82c4c0feb
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-03 22:33:51 +0530

    Remove unused function and clean up layout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,21 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
+
+  const name = () => {
+    console.log('Something Went Wrong !')
+
+
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen 
-        name="index" 
+      <Stack.Screen
+        name="index"
       />
-     
+
+
+
     </Stack>
   );
 }
