ffe11bdeb22ad5080aee3e4d39442377a35e5002
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-03 22:31:49 +0530

    Refactor index component structure and styles for improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -5,20 +5,20 @@ export default function index() {
 
   return (
     <View style={styles.container}>
-      <Image source={require("@/assets/images/logo.png")} style={styles.logo}/>
+      <Image source={require("@/assets/images/logo.png")} style={styles.logo} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
-  logo:{
-    height:70,
-    width:250
+  logo: {
+    height: 70,
+    width: 250
   },
 })
