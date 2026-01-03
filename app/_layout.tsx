deb6c2c929f3ae64683fdaab007868de377a2dde
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-03 22:36:40 +0530

    Refactor layout component for improved structure and readability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,11 @@ import { Stack } from 'expo-router';
 
 export default function Layout() {
 
-  const name = () => {
-    console.log('Something Went Wrong !')
-
-
-  }
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name="index"
       />
-
-
-
     </Stack>
   );
 }
