586fa01436d39da0d9fb55cddd236fd8428e66ed
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-14 10:30:03 +0530

    Add camera functionality to SigninScreen with photo capture feature

--- app/screens/signinscreen.tsx
+++ app/screens/signinscreen.tsx
@@ -1,13 +1,20 @@
 import { BarcodeScanningResult, CameraType, CameraView, useCameraPermissions } from 'expo-camera';
 import { useRouter } from 'expo-router';
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image, Text, View } from 'react-native';
+// import imageView
 
 export default function SigninScreen() {
 
   const router = useRouter();
 
+  // using ref for camera view
+  const cameraRef = useRef<CameraView>(null);
+
   const [permission, requestPermission] = useCameraPermissions();
+
+  // to store photo uri
+  const [photos, setphoto] = useState<string | undefined>(undefined);
 
   // for camera facing start with back camera
   const [facing, setfacing] = useState<CameraType>('back');
@@ -26,6 +33,12 @@ export default function SigninScreen() {
     )
   }
 
+  // get photos using ref
+  const takePhoto = async () => {
+    const photo = await cameraRef.current?.takePictureAsync();
+    setphoto(photo?.uri);
+  }
+
   return (
     <View>
       <Text style={{ fontSize: 86 }}>signinscreen</Text>
@@ -33,18 +46,23 @@ export default function SigninScreen() {
       {/* Facing using can access front or back camera */}
       <CameraView barcodeScannerSettings={{
         barcodeTypes: ["qr"],
-      }} 
-
-      // log the barcode result
-      onBarcodeScanned={(result) => {
-        console.log(result);
       }}
 
-      // styles
-      style={{ width: 550, height: 400 }} facing={facing} />
+        // use to access camera methods
+        ref={cameraRef}
+
+        // log the barcode result
+        onBarcodeScanned={(result) => {
+          console.log(result);
+        }}
+
+        // styles
+        style={{ width: 550, height: 400 }} facing={facing} />
 
       {/* switch camera facing */}
       <Text style={{ fontSize: 20, padding: 20, color: 'blue' }} onPress={toggleFacing}>Toggle Facing</Text>
+      <Text style={{ fontSize: 20, padding: 20, color: '#ffffff', backgroundColor: '#ff0000' }} onPress={takePhoto}>Take Photo</Text>
+      {photos && <Image source={{ uri: photos }} style={{ width: 200, height: 200 }} />}
     </View>
   )
 }
